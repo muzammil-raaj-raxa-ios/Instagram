77422de6e1de7205e3e19de8b86aa4b701cdaf10
muzammil-raaj-raxa-ios <raajadobemania@gmail.com>
2024-07-23 15:53:42 +0500

    Initial Commit

---
+++ Instagram/Controllers/CoreTabs/HomeVC.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Instagram
+//
+//  Created by Mag isb-10 on 19/07/2024.
+//
+
+import UIKit
+
+class HomeVC: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
